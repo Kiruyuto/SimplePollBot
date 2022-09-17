bc8e41e3eabd36d65216e131f4627d9b8f191229
Hubert Zieliński <h.zielinski@sellpander.pl>
2022-09-17 19:32:35 +0200

    New features

---
+++ src/emojiOptions.js
@@ -0,0 +1,12 @@
+const emojiOptions = {
+  '1️⃣': 'Środa (Reset)',
+  '2️⃣': 'Czwartek',
+  '3️⃣': 'Piątek',
+  '4️⃣': 'Sobota',
+  '5️⃣': 'Niedziela',
+  '6️⃣': 'Poniedziałek',
+  '7️⃣': 'Wtorek (Last day)',
+  '❌': 'Odpadam w tym tyg.',
+};
+
+module.exports = emojiOptions;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-const {
-  Client,
-  GatewayIntentBits,
-  ActivityType,
-  EmbedBuilder,
-} = require('discord.js');
-const dt = require('date-fns');
+const { Client, GatewayIntentBits, ActivityType, EmbedBuilder } = require('discord.js');
+const { DateTime } = require('luxon');
+const IDs = require('./id');
+const emojiDays = require('./emojiOptions');
 require('dotenv').config();
 
 const client = new Client({
@@ -16,8 +13,10 @@ const client = new Client({
     GatewayIntentBits.DirectMessages,
     GatewayIntentBits.MessageContent,
   ],
-  partials: ['CHANNEL'],
-  restTimeOffset: 0,
+  partials: ['CHANNEL', 'GUILD_MEMBER', 'MESSAGE', 'REACTION', 'USER'],
+  rest: { offset: 0 },
+  shards: 'auto',
+  allowedMentions: { parse: ['users', 'roles'], repliedUser: true },
 });
 
 client.on('ready', async () => {
@@ -34,84 +33,154 @@ client.on('ready', async () => {
   client.application.fetch();
 });
 
-const dalgomId = '212328101999411201';
+let now = DateTime.now();
 const grupa3ChannelId = '1001601818444431501';
-let latestPoolId = '';
-
-let now = new Date();
-let daysAfterLastThursday = -7 + 3 - now.getDay();
-let lastWednsday = new Date(
-  now.getTime() + daysAfterLastThursday * 24 * 60 * 60 * 1000
-);
-let nextday = new Date(
-  lastWednsday.getFullYear(),
-  lastWednsday.getMonth(),
-  lastWednsday.getDate() + 6
-);
+let pollId = '';
 
 client.on('messageCreate', async (message) => {
-  if (message.channel.isDMBased()) return;
   try {
-    if (message.content == '!poll') {
-      if (message.channel.id != grupa3ChannelId) {
-        message.author
-          .send(
-            'Nie możesz tego użyć na tym kanale 🙉\nSpróbuj na: <#1001601818444431501>'
-          )
-          .catch((e) => {
-            console.log(`Error: ${e.message}`);
-          });
+    if (message.channel.isDMBased()) return;
+
+    if (message.content.toLowerCase() == '!poll') {
+      if (message.channel.id == IDs.grupa3ChannelId) {
+        //TODO: !=
+        message.author.send(`Nie możesz tego użyć na tym kanale 🙉\nSpróbuj na: <#${IDs.grupa3ChannelId}>`).catch(e);
         return;
-      } else if (
-        message.author.id == dalgomId ||
-        message.author.id == client.application.owner.id
-      ) {
-        let msgTimestamp = Date.now();
-        if (dt.getUnixTime(msgTimestamp) >= dt.getUnixTime(nextday)) {
-          const pollEmbed = new EmbedBuilder();
-          pollEmbed.addFields({
-            name: 'Wartości',
-            value:
-              '1️⃣ - Środa (Reset)\n2️⃣ - Czwartek\n3️⃣ - Piątek\n4️⃣ - Sobota\n5️⃣ - Niedziela\n6️⃣ - Poniedziałek\n7️⃣ - Wtorek (Last day)\n<a:Fill:1018928990616027156> - Bez znaczenia ',
+      } else if (message.author.id == IDs.dalgom || message.author.id == client.application.owner.id) {
+        let wednesday;
+        if (now.weekday == 1 || now.weekday == 2) {
+          wednesday = DateTime.fromObject({
+            year: now.getFullYear,
+            weekNumber: now.weekNumber - 1,
+            weekday: 3,
+          });
+        } else {
+          wednesday = DateTime.fromObject({
+            year: now.getFullYear,
+            weekNumber: now.weekNumber,
+            weekday: 3,
+          });
+        }
+
+        const dateDiff = wednesday.diffNow('days').toObject().days;
+
+        if (dateDiff <= 6) {
+          //TODO: <= -6
+          let staticMembers = '';
+          Object.keys(IDs).forEach((key) => {
+            staticMembers += `[❓] <@${IDs[key]}>\n`;
           });
 
-          pollEmbed.setColor('#ff0000');
-          pollEmbed.setTitle('Vykas HardMode');
+          let dayValues = '';
+          for (const [k, v] of Object.entries(emojiDays)) {
+            dayValues += `${k} - ${v}\n`;
+          }
 
-          pollEmbed.setDescription(
-            `<t:${dt.getUnixTime(lastWednsday)}:D> - <t:${dt.getUnixTime(
-              nextday
-            )}:D>`
-          );
+          const pollEmbed = new EmbedBuilder()
+            .setColor('#ff0000')
+            .setTitle('Vykas HardMode')
+            .setURL('https://i.imgur.com/cfdsW3C.png')
+            .setDescription(`<t:${wednesday.toMillis() / 1000}:D> - <t:${wednesday.plus({ days: 6 }).toMillis() / 1000}:D>`)
+            .setFooter({ text: `❓ - Brak głosu | ✅ - Głos oddany` })
+            .setTimestamp(DateTime.now().toMillis())
+            .addFields(
+              {
+                name: 'Wartości:',
+                value: dayValues,
+                inline: true,
+              },
+              {
+                name: 'Status głosów:',
+                value: staticMembers,
+                inline: true,
+              }
+            );
 
-          await message.channel
-            .send({ embeds: [pollEmbed] })
-            .then(async (msg) => {
-              await msg.react('1️⃣');
-              await msg.react('2️⃣');
-              await msg.react('3️⃣');
-              await msg.react('4️⃣');
-              await msg.react('5️⃣');
-              await msg.react('6️⃣');
-              await msg.react('7️⃣');
-              await msg.react('<a:Fill:1018928990616027156>');
-              latestPoolId = msg.id;
+          await message.channel.send({ embeds: [pollEmbed] }).then(async (msg) => {
+            Object.keys(emojiDays).forEach((key) => {
+              msg.react(key);
             });
+
+            pollId = msg.id;
+            now = DateTime.now();
+          });
         } else {
           message.reply(
-            `Po co robić kolejną ankiete? 🤔\nUtworzyć nową będziesz mógł dopiero we Wtorek.. (<t:${dt.getUnixTime(
-              nextday
-            )}:R>)`
+            `Utworzyć głosowanie będziesz mógł dopiero we Wtorek (<t:${wednesday.plus({ days: 6 }).toMillis() / 1000}:D>`
           );
         }
       } else {
-        await message.author.send('Nie możesz tego użyć 😇').catch((e) => {
-          console.log(`Error: ${e.message}`);
-        });
+        await message.author.send('Nie możesz tego użyć 😇\n(Allowed: `Dalgom`, `Kiru`)').catch(e);
       }
     }
   } catch (e) {
     console.log(`Wystąpił błąd: ${e}`);
+  }
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+  if (user.bot) return;
+  if (reaction.message.id == pollId) {
+    if (emojiDays[reaction.emoji.name] == undefined) return;
+
+    await reaction.message.fetch(reaction.message.id).then((msg) => {
+      let votesEmbedValue = msg.embeds[0].fields[1].value;
+      let max = {};
+
+      var sorted = msg.reactions.cache.sort((a, b) => b.count - a.count);
+      sorted.each((r) => {
+        if (r.count >= sorted.first().count) max[r.emoji.name] = r.count;
+      });
+
+      let msgStr = `Optymalny termin:\n`;
+      for (const [k, v] of Object.entries(max)) {
+        msgStr += `\`${emojiDays[k]} - ${v - 1} reakcje\`\n`;
+      }
+
+      let staticMembers = '';
+      Object.keys(IDs).forEach((key) => {
+        staticMembers += `[x] <@${IDs[key]}>\n`;
+      });
+
+      votesEmbedValue = votesEmbedValue.replace(`[❓] <@${user.id}>`, `[✅] <@${user.id}>`);
+
+      let embed = msg.embeds[0];
+      embed.fields[1] = { name: msg.embeds[0].fields[1].name, value: votesEmbedValue, inline: true };
+      msg.edit({ content: msgStr, embeds: [embed] });
+    });
+  }
+});
+
+client.on('messageReactionRemove', async (reaction, user) => {
+  if (user.bot) return;
+  if (reaction.message.id == pollId) {
+    if (emojiDays[reaction.emoji.name] == undefined) return;
+
+    await reaction.message.fetch(reaction.message.id).then((msg) => {
+      let votesEmbedValue = msg.embeds[0].fields[1].value;
+      let max = {};
+
+      var sorted = msg.reactions.cache.sort((a, b) => b.count - a.count);
+      sorted.each((r) => {
+        if (r.count >= sorted.first().count) max[r.emoji.name] = r.count;
+      });
+
+      let msgStr = `Optymalny termin:\n`;
+      for (const [k, v] of Object.entries(max)) {
+        msgStr += `\`${emojiDays[k]} - ${v - 1} reakcje\`\n`;
+      }
+
+      let staticMembers = '';
+      Object.keys(IDs).forEach((key) => {
+        staticMembers += `[x] <@${IDs[key]}>\n`;
+      });
+
+      votesEmbedValue = votesEmbedValue.replace(`[✅] <@${user.id}>`, `[❓] <@${user.id}>`);
+
+      let embed = msg.embeds[0];
+      embed.fields[1] = { name: msg.embeds[0].fields[1].name, value: votesEmbedValue, inline: true };
+      msg.edit({ content: '', embeds: [embed] });
+    });
   }
 });
 
